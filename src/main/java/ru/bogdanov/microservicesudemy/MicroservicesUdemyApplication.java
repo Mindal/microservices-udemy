27c3e2a00501995e97ce4b902db91d746b681d96
Andrei Bogdanov <andrei_bogdanov@epam.com>
2017-10-10 16:25:42 +0300

    init project

---
+++ src/main/java/ru/bogdanov/microservicesudemy/MicroservicesUdemyApplication.java
@@ -0,0 +1,12 @@
+package ru.bogdanov.microservicesudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesUdemyApplication.class, args);
+    }
+}
